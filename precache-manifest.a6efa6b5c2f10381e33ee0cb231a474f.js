42bb040db3abf2e1e57070e7f62b5af3edabec16
Vidit Shah <viditisonline@gmail.com>
2018-11-06 11:57:43 +0530

    Updates

---
+++ precache-manifest.a6efa6b5c2f10381e33ee0cb231a474f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cd08478c3e8c807ff060",
+    "url": "/static/js/main.cd08478c.chunk.js"
+  },
+  {
+    "revision": "e9b7c5b2fdca2fdd5fe7",
+    "url": "/static/js/1.e9b7c5b2.chunk.js"
+  },
+  {
+    "revision": "cd08478c3e8c807ff060",
+    "url": "/static/css/main.4c648178.chunk.css"
+  },
+  {
+    "revision": "c4f25b7e3900638513f71c6173fd51c6",
+    "url": "/index.html"
+  }
+];
