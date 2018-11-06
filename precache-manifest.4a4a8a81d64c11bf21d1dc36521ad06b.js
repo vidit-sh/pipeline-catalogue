01708b0ee632f154fa8b6838d453f88a3ba48c60
Vidit Shah <viditisonline@gmail.com>
2018-11-06 12:03:14 +0530

    Updates

---
+++ precache-manifest.4a4a8a81d64c11bf21d1dc36521ad06b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38da9292cd66219c39a2",
+    "url": "/pipeline-catalogue/static/js/runtime~main.38da9292.js"
+  },
+  {
+    "revision": "cd08478c3e8c807ff060",
+    "url": "/pipeline-catalogue/static/js/main.cd08478c.chunk.js"
+  },
+  {
+    "revision": "e9b7c5b2fdca2fdd5fe7",
+    "url": "/pipeline-catalogue/static/js/1.e9b7c5b2.chunk.js"
+  },
+  {
+    "revision": "cd08478c3e8c807ff060",
+    "url": "/pipeline-catalogue/static/css/main.4c648178.chunk.css"
+  },
+  {
+    "revision": "9a20b14bc7a21c00ed205d2682889819",
+    "url": "/pipeline-catalogue/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a6efa6b5c2f10381e33ee0cb231a474f.js"
+  "/pipeline-catalogue/precache-manifest.4a4a8a81d64c11bf21d1dc36521ad06b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/pipeline-catalogue/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
