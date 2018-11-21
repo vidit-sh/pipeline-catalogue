72180e86067c9b2fdf10ceefaea06ccd76bc6c2b
Vidit Shah <viditisonline@gmail.com>
2018-11-21 13:09:57 +0000

    Updates

--- precache-manifest.cbb80ac225244ca58ec8c12b8eeb809b.js
+++ precache-manifest.cbb80ac225244ca58ec8c12b8eeb809b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/pipeline-catalogue/static/js/runtime~main.38da9292.js"
   },
   {
-    "revision": "7c7b8134f8a5a0f4c697",
-    "url": "/pipeline-catalogue/static/js/main.7c7b8134.chunk.js"
+    "revision": "f7254d903384dca022ba",
+    "url": "/pipeline-catalogue/static/js/main.f7254d90.chunk.js"
   },
   {
     "revision": "0cc35c41323d60203ef3",
     "url": "/pipeline-catalogue/static/js/1.0cc35c41.chunk.js"
   },
   {
-    "revision": "7c7b8134f8a5a0f4c697",
+    "revision": "f7254d903384dca022ba",
     "url": "/pipeline-catalogue/static/css/main.57d0820c.chunk.css"
   },
   {
-    "revision": "1b9d5b6bc1308350a109aa2ef0417992",
+    "revision": "ea24b5cac94c28c2c990302a4c411d5d",
     "url": "/pipeline-catalogue/index.html"
   }
 ];
